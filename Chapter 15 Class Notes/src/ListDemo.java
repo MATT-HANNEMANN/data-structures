3c4a02299cb85ecfdd43c9bf3da60ec6c8bacd1e
MATT-HANNEMANN <mjhannemann@stu.naperville203.org>
2025-08-25 13:17:34 -0500

    Took Notes on Linked lists

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,28 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
-        
+        LinkedList <String> Staff = new LinkedList<>();
+        Staff.addLast("Tony");
+        Staff.addLast("Natasha");
+        Staff.addLast("Peter");
+        Staff.addLast("Steve");
+
+        System.out.println(Staff);
+
+        //List is Currently: [Tony, Natasha, Peter, Steve]
+        /* 
+         * The ListIterator method creates a new list iterator
+         * That is positioned at the head of the list.
+         * the | is used to represent the Iterator position. 
+        */
+
+        ListIterator<String> iterator = staff.listIterator(); // | Tony, Natasha, Peter, Steve
+
+        /* The next method advances teh iterator OVER the next element in the list. */
+        iterator.next(); // Tony | Natasha, Peter, Steve 
+
+        /* The next method also returns the element that the iterator passes over */
+        String avenger = iterator.next(); // Natasha | Peter, Steve
+        System.out.println(avenger); //should print Natasha
     }
 }
