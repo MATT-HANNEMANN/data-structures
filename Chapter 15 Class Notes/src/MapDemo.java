83e6a66cfa5fe0d2950964ca9171aee0efaf2454
MATT-HANNEMANN <mjhannemann@stu.naperville203.org>
2025-09-09 13:22:26 -0500

    took notes on Maps

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,27 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        Map<String, Color> favColors = new HashMap<>();
+
+        favColors.put("Sreeram", Color.GREEN);
+        favColors.put("Cam", Color.BLUE);
+        favColors.put("Nimai", Color.RED);
+
+        // Two different elements with the same value
+        favColors.put("Kaitlyn", Color.GREEN);
+
+        // The same key CANNOT have 2 different values
+        // Using put on a key that exists, changes the value
+        favColors.put("Cam", Color.ORANGE);
+
+        // Create a set of keys ina a map
+        Set<String> keys = favColors.keySet();
+        for (String key: keys) {
+            // [name] ([hashCode]): [color]
+            System.out.println(key + "(" + key.hashCode() + "): " + favColors.get(key));
+        }
+
+
+
     }
 }
