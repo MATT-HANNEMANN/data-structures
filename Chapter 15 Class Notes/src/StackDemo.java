831dc6057d7df3b480faab607b6ddce77e2526af
MATT-HANNEMANN <mjhannemann@stu.naperville203.org>
2025-09-15 13:07:15 -0500

    Took notes on StackDemo

--- Chapter 15 Class Notes/src/StackDemo.java
+++ Chapter 15 Class Notes/src/StackDemo.java
@@ -9,5 +9,28 @@ public class StackDemo
 {
     public static void main(String[] args)
     {
+        Stack<String> commands = new Stack<>();
+
+        // push a bunch of commands on the top of the stack
+        commands.push("Insert: 'Hello'");
+        commands.push("Insert: ','");
+        commands.push("Insert: ' '");
+        commands.push("Insert: 'World'");
+        commands.push("Insert: '?'");
+        commands.push("Delete: '?'");
+        commands.push("Insert: '!'");
+
+
+        System.out.println(commands);
+
+
+        for (int i = 0;i<4; i++)
+            {System.out.println("Undo: " + commands.pop());}
+
+        
+            System.out.println(commands);
+
+
+
     }
 }
