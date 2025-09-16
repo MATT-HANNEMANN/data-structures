b24764ff6ae494ca1fef0b53bfeb7a5d37564e2a
MATT-HANNEMANN <mjhannemann@stu.naperville203.org>
2025-09-16 13:23:04 -0500

    Took notes on queues

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,32 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        //Queue<String> students = new PriorityQueue<>();
+        //students.add("Neel");
+        //students.add("Jonathon");
+        //students.add("Cam");
+        //students.add("Kaitlyn");
+        //students.add("Dylan");
+
+        // The next highest priority object is moved to the front of the queue
+        //when the head of the queue is removed
+        //while (students.size() > 0)
+        //    {System.out.println(students.remove());}
+
+
+
+        Queue<WorkOrder> toDo = new  PriorityQueue<>();
+
+        toDo.add(new WorkOrder(3, "Water plants"));
+        toDo.add(new WorkOrder(2, "make dinner"));
+        toDo.add(new WorkOrder(1, "Conquer the world"));
+        toDo.add(new WorkOrder(9, "play videogames"));
+        toDo.add(new WorkOrder(1, "study for the SE 1 test"));
+
+        System.out.println(toDo);
+
+        while (toDo.size() > 0)
+            {System.out.println(toDo.remove());}
+
     }
 }
--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,39 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
-    }
+        Queue<String> jobs = new LinkedList<>();
+
+        jobs.add("Harry: Quarter 2 Report");
+        jobs.add("Connor: Recipe got Banana Bread");
+        jobs.add("Katherine: Top Secret Document");
+        
+        System.out.println("Printing: " + jobs.remove());
+        
+        // add some more print jobs
+        jobs.add("Vardhan: Grocery List");
+        jobs.add("Katherine: Really Top Secret Document");
+        jobs.add("Katherine: Can I get fired for this?");
+        
+        System.out.println("Printing: " + jobs.remove());
+        System.out.println("Printing: " + jobs.remove());
+
+        jobs.add("Boss: Katherine Termination Letter");
+
+        //print out the rest of the queue
+        //while (!jobs.isEmpty())
+        //{
+        //    String job = jobs.remove();
+        //    System.out.println("Printing: " + job);
+        //}
+        //
+        // OR
+
+        for (int i= 0; i < jobs.size(); i++ )
+        {
+            String job = jobs.remove();
+            System.out.println("Printing: " + job);
+            jobs.add(job);
+        }
+        
+}
 }
