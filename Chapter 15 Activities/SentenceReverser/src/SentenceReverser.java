a07a8c7cb5f96bd8c85af5cc6749d41fbc0d797f
MATT-HANNEMANN <mjhannemann@stu.naperville203.org>
2025-09-18 13:30:01 -0500

    I am going to have a brain aneurysm

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -14,10 +14,87 @@ public class SentenceReverser
     */
     public static String reverse(String sentence)
     {
-    	Scanner scanner = new Scanner(sentence);
+
+    // "Mary had a little lamb" - "lamb little a had Mary"
+
     	
-        // Complete this method. Use a Stack.
-        ...
+        Stack<String> ReverseSentence1 = new Stack<>();
+        Stack<String> ReverseSentence2 = new Stack<>();
+
+
+
+        
+
+
+
+        String word = "";
+        int sentenceMarker = 1;
+       for (int i = 0; i < sentence.length(); i++)
+        {
+
+            if(sentence.charAt(i) != ' ') {
+                word += sentence.charAt(i);}
+      
+            
+               
+
+
+
+
+                else 
+                {
+                    
+                    if (sentenceMarker == 1) 
+                       { ReverseSentence1.push(word);
+                        word = "";}
+           
+
+              
+                     else if (sentenceMarker == 2) 
+                      { ReverseSentence2.push(word);
+                        word = "";}
+
+                }
+
+
+
+                    if (sentence.charAt(i) != '.') 
+                        {sentenceMarker = 2;}
+
+
+
+           
+           }
+
+
+
+
+
+
+        }
+
+        {
+        String ReversedSentenceString = "";
+
+
+
+
+        for (String aWord: ReverseSentence1) {
+            ReversedSentenceString += aWord + " ";
+        }
+
+        ReversedSentenceString += ". ";
+
+        for (String bWord: ReverseSentence2) {
+            ReversedSentenceString += bWord + " ";}
+        
+
+        return ReversedSentenceString;
+        
+
+        //return "hello";
+
+        //...
 
 
 
@@ -25,4 +102,3 @@ public class SentenceReverser
 
 
     }
-}
