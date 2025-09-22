54c8ec8ed08f2abe28964609ae5541682798e545
MATT-HANNEMANN <mjhannemann@stu.naperville203.org>
2025-09-22 13:17:09 -0500

    I have to finish this at home

--- Chapter 15 Activities/FloodFill/src/Grid.java
+++ Chapter 15 Activities/FloodFill/src/Grid.java
@@ -2,14 +2,32 @@ public class Grid
 {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    . . .
+    
+    private int[][] grid = new int[10][10];
 
     /**
      * Flood fill, starting with the given row and column.
     */
     public void floodfill(int row, int column)
     {
-        . . .
+        public void floodFill(int startRow, int startCol) {
+        Stack<Pair> stack = new Stack<>();
+        int fillValue = 1;
+        stack.push(new Pair(startRow, startCol));
+        while (!stack.isEmpty()) 
+            {
+                Pair p = stack.pop();
+                int r = p.row, c = p.col;
+                if (r >= 0 && r < 10 && c >= 0 && c < 10 && grid[r][c] == 0) 
+                {
+                    grid[r][c] = fillValue++;
+                    stack.push(new Pair(r-1, c)); // north
+                    stack.push(new Pair(r+1, c)); // south
+                    stack.push(new Pair(r, c-1)); // west
+                    stack.push(new Pair(r, c+1)); // east
+                }
+            }
+        }
     }
 
     @Override
